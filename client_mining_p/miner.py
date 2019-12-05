ddc47d587ec3365c577f83c3bef79eff59199e3a
yusufnafey <yusufnafey@yahoo.com>
2019-12-05 15:07:48 -0800

    almost done with mvp, getting error

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,6 +4,7 @@ import requests
 import sys
 import json
 
+DIFFICULTY = 3
 
 def proof_of_work(block):
     """
@@ -13,7 +14,12 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(block, sort_keys=True).encode()
+    proof = 0
+
+    while not self.valid_proof(block_string, proof):
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +33,12 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    if guess_hash[:DIFFICULTY] == '0' * DIFFICULTY:
+        print(guess)
+    return guess_hash[:DIFFICULTY] == '0' * DIFFICULTY
 
 
 if __name__ == '__main__':
@@ -57,8 +68,10 @@ if __name__ == '__main__':
 
         # TODO: Get the block from `data` and use it to look for a new proof
         # new_proof = ???
+        new_proof = proof_of_work(last_block.get('last_block'))
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
+        
         post_data = {"proof": new_proof, "id": id}
 
         r = requests.post(url=node + "/mine", json=post_data)
@@ -67,4 +80,8 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data.get('message') == 'New Block Forged':
+            coins_mined += 1
+            print("Total coins mined: " + str(coins_mined))
+        else:
+            print(data.get('message'))
